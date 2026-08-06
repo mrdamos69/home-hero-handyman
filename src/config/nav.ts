aca5df0af5b58a46787379b85787d733b20d56ab
Dmitrii Chichkanov <86652230+mrdamos69@users.noreply.github.com>
2026-08-05 17:54:52 -0700

    Create nav.ts

---
+++ src/config/nav.ts
@@ -0,0 +1,9 @@
+export const navLinks = [
+  { href: "/", label: "Home" },
+  { href: "/building-maintenance", label: "Building Maintenance" },
+  { href: "/welding", label: "Welding" },
+  { href: "/services", label: "Services" },
+  { href: "/gallery", label: "Gallery" },
+  { href: "/about", label: "About" },
+  { href: "/contact", label: "Contact" },
+] as const;
